e9d6c74bf70b4116ec7ce99c10fb0cb0ac37d9f6
zhangpei <13488233237@163.com>
2016-06-01 21:43:24 +0800

    finish collect_same_elements function

--- practices/section_1/practice_1/practice.js
+++ practices/section_1/practice_1/practice.js
@@ -1,5 +1,15 @@
 function collect_same_elements(collection_a, collection_b) {
   //在这里写入代码
+  var arr = [];
+  for(var i = 0; i < collection_a.length; i++){
+     var s=collection_a[i];
+     for(var j = 0; j < collection_b.length; j++){
+       if(s == collection_b[j]){
+         arr.push(s);
+       }
+     }
+  }
+  return arr;
 }
 
 module.exports = collect_same_elements;
